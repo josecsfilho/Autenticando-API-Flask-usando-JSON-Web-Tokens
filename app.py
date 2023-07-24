5cde423210fc28b0c8a3c499ce1d9dffc4fa8091
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-07-24 20:51:07 -0300

    API flask usando json web token

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request, make_response
+import jwt
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')  # http://127.0.0.1:5000/route?token=
+
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 403
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 403
+
+            return f(*args, **kwargs)
+
+@app.route('/desprotegido')
+def desprotegido():
+    return ''
+
+@app.route('/protegido')
+def protegido():
+    return ''
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if auth and auth.password == 'password':
+        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+
+if __name__ == '__name__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
